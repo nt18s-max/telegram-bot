1d254ec7572f8ffae087d8c874a83ee1d7a372b2
nt18s-max <naifsaba99@gmail.com>
2026-02-24 19:37:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -490,7 +490,8 @@ class KeepAlive(BaseHTTPRequestHandler):
         pass
 
 def run_server():
-    server = HTTPServer(("0.0.0.0", 8080), KeepAlive)
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), KeepAlive)
     server.serve_forever()
 
 # ----- بدء البوت -----
